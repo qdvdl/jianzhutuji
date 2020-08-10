f1d5ef6865b06b4ec5f3177ca09afdc8b49d020b
qdvdl <2967547623@qq.com>
2020-08-10 20:38:50 +0800

    测试

--- src/qdvdl/config.js
+++ src/qdvdl/config.js
@@ -1,5 +1,5 @@
 import store from '../store';
-//测试提交  终端提交测试啥都发啥地
+//测试提交  终端测试
 let {userInfo,operation,siteinfo,login}=store.getters.getData;
 // console.log(operation);
 let Verification=(name)=>{
